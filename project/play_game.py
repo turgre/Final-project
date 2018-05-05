5c9b30e657da042a74cee63ce36f20e1553dbc2c
Turgre <dsfjksd@gmail.com>
2018-05-05 14:11:51 -0400

    Adding code to play the game

---
+++ project/play_game.py
@@ -0,0 +1,30 @@
+import time
+import random
+
+def play_game():
+    print("\n A stiff chill sweeps through your tent, you awaken shivering and upset."
+        "\n Rubbing the sleep from your eyes, you grab a journal adding a tally mark to a page already littered with those same faded marks.",
+        "\n Today marks your 50th day since your separation from the group.")
+    time.sleep(10)
+    print("/n)
+    
+    print("\n Gathering your strength, you shift slowly from the makeshift bed on the floor.",
+        "\n A yawn spills out of your mouth as you pull on a thick winter fur coat and walk towards the opening to your tent.",
+        "\n You move to let yourself out but find the entrance slightly open, that explains the wind in the tent.")
+
+play_game()
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
