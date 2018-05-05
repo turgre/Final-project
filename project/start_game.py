e7ad35cb3e24bfbf77c1267e053c9b5ced5bbcd3
Turgre <dsfjksd@gmail.com>
2018-05-05 17:10:28 -0400

    Adding better loops

--- project/start_game.py
+++ project/start_game.py
@@ -3,7 +3,11 @@ import sys
 import random
 import play_game
 
+
+"""This function bring the player to a start screen and begins the game"""
 def start_game():
+    #Code for loop https://stackoverflow.com/questions/32928143/how-to-continue-asking-user-for-input-until-it-is-considered-valid
+    start = None
     print("★★★★★★★★★★★★★★★★★★★★★★★★"
         "\n Welcome to a text based adventure!",
           "\n What would you like to do?",
@@ -12,15 +16,15 @@ def start_game():
           "\n★★★★★★★★★★★★★★★★★★★★★★★★")
     
     start = input("Enter option here: ").lower()
+
+    while start not in ("start", "quit"):
+        if start is not None:
+            print("\nInvaild reponse")
+        start = input("Enter option here: ").lower()
+            
     if start == "start":
         play_game.play()   
     elif start == "quit":
         sys.exit()
-    while start != "start" or "quit":
-        print("Not vaild")
-        start = input("Enter option here: ").lower()
-        if start == "start":
-             play_game.play()  
-        elif start == "quit":
-            sys.exit()
+        
 start_game()
